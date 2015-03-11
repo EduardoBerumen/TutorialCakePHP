54b287f7bb93a93783699adec65a3c7ab7ec4378
Eduardo Berumen <eduardo_berumen@hotmail.com>
2015-03-11 13:34:21 -0600

    Segundo Cambio

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -4,6 +4,16 @@
 class UsersController extends AppController {
 	var $layout = 'login';
 	var $uses = array();
+	var $components = array('Auth', 'Session');
+    var $helpers = array('Html', 'Form', 'Session');
+
+    function beforeFilter() {
+        //Configure AuthComponent
+        $this->Auth->authorize = 'actions';
+        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
+        $this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'logout');
+        $this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
+    }
 
 	function index(){
 
